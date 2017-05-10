366f39fd1a1184b90efe1f08f06e149b93749643
Akuilner <och5@comcast.net>
2017-05-10 11:28:42 -0400

    Here are my exercises for week 1 sorry I didn’t get them to you on time I was not aware of how SourceTree worked.

---
+++ Week 1 Exercises/Exercise 4.17/src/Exercise4_17.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Exercise4_17 {
+
+	public static void main(String[] args) {
+	
+	System.out.println("Enter a year: ");
+	double selectedYear = input.nextDouble();
+	
+	System.out.println("Enter a month: ");
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Week 1 Exercises/Exercise 4.2/src/Exercise4_2.java
@@ -0,0 +1,33 @@
+	import java.util.Scanner;
+	public class Exercise4_2 {
+	public static void main(String[] args) {
+		
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter point 1 (latitude and longitude) in degrees: ");
+	double latitudeOne = input.nextDouble();
+	double longitudeOne = input.nextDouble();
+	
+	System.out.println("Enter point 2 (latitude and longitude) in degrees: ");
+	double latitudeTwo = input.nextDouble();
+	double longitudeTwo = input.nextDouble();
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
